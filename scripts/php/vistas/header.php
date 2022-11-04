b578fdf9aebd13393763598fd502b0104f249d71
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2022-11-04 13:06:07 -0600

    Se corrigieron cosas del header

--- scripts/php/vistas/header.php
+++ scripts/php/vistas/header.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://kit.fontawesome.com/dae357c832.js" crossorigin="anonymous"></script>
+    
     <title>Autos Amistosos</title>
 </head>
 <body>
