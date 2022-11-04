31c8ec6cdaff9bc94f6011308f8c1a79de567883
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2022-11-04 12:49:13 -0600

    Se creó el header.php

---
+++ scripts/php/vistas/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/dae357c832.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+<nav id="header" class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                Autos Amistosos</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#carrusel">Ventas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#ponentes">Ponentes Principales</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#lugarFecha">Lugar y Fechas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#calendario">Calendario</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#inscribe">Inscribe a tú equipo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-outline-success" data-bs-toggle="" href="#"><strong>Comprar
+                                Entradas</strong></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
