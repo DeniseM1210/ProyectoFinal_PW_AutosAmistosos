8c967d2071356f5f023d358f664e9e901ef4877b
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2022-11-04 13:24:11 -0600

    Se modificaron cosas del header

--- scripts/php/vistas/ventas.php
+++ scripts/php/vistas/ventas.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://kit.fontawesome.com/dae357c832.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="../../../css/main.css">
     <title>Ventas</title>
 </head>
 <body>
