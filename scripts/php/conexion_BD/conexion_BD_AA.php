57931e9505829af8f2a401926daeaf43118b320f
Cesar Ramirez <cesar1xd1@gmail.com>
2022-11-04 20:31:32 -0600

    Se cambio la conexion a MySQL y se añadio la carpeta de controlador (carpeta de los DAO's)

--- scripts/php/conexion_BD/conexion_BD_AA.php
+++ scripts/php/conexion_BD/conexion_BD_AA.php
@@ -1,38 +1,34 @@
 <?php
     class Conexion{
+        //Modificar Datos para BAse de datos en WEbHOST
         private $conexion;
         private $host="localhost";
-        private $usuario = "aa";
+        private $usuario = "";
         private $contraseña = "1234";
         private $db = "autos_amistosos"; 
 
         public function __construct(){
-                try{
-                    $this->conexion = new PDO("pgsql:host=$this->host; dbname=$this->db", $this->usuario,$this->contraseña );
-                    echo "Conexion con exito";
-                }catch(PDOException $exp){
-                    echo "conexion erronea";
-                }
-                
-                
-
-                
-            
-            
-
-                
-            
+            $this->conexion = mysqli_connect($this->host,$this->usuario,$this->contraseña,$this->db);
+            if(!$this->conexion){
+                die("Error en conexion a BD" . mysqli_connection_error() . mysqli_connect_errno());
+            }else{
+                echo "Conexion exitosa"; //Borar esto
+            }
         }
-
-        public function getConn(){
+        
+        public function getConexion(){
             return $this->conexion;
+        }      
+    
         }
+
+       
        
 
 
 
 
 
-    }
+    
 
 ?>
